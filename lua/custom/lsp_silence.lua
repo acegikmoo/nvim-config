d1e6745d66cf47fac823c0bc4291d747bb9cdcbd
rubairakib <rubairakib11@gmail.com>
2025-11-21 14:56:11 +0600

    remove silence_lsp

--- lua/custom/lsp_silence.lua
+++
@@ -1,9 +0,0 @@
-
--- silently ignore nvim-lspconfig deprecation warnings
-local notify_save = vim.notify
-vim.notify = function(msg, log_level, opts)
-  if type(msg) == "string" and msg:match("deprecated") then
-    return  -- ignore the warning
-  end
-  notify_save(msg, log_level, opts)
-end
