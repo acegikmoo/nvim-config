de1337cd343ac7a1325f14440fae6f83ac5f50f6
0xdolores <rubairakib11@gmail.com>
2026-02-07 10:38:12 +0600

    fuck fmt

--- lua/configs/conform.lua
+++ lua/configs/conform.lua
@@ -6,9 +6,16 @@ local options = {
     html = { "prettier" },
     markdown = { "prettier" },
   },
-  format_on_save = {
-    timeout_ms = 2000, 
-    lsp_fallback = false,
-  },
+  format_on_save = function()
+    -- Check the global flag
+    if vim.g.format_on_save then
+      return {
+        timeout_ms = 2000,
+        lsp_fallback = true,
+      }
+    end
+    -- Return nil to skip formatting
+    return nil
+  end,
 }
 return options
